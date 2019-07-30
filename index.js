6eee93436a4e2b705364bb36188e7082236632fc
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-07-30 19:25:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.loadCommands();
 client.on('ready', () => {
   console.log(`READY Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   client.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`, "Ready", "event");
-  client.user.setActivity(`Pokemon Hunting`, { url: "https://www.twitch.tv/hypebot"}, { type: "STREAMING"});
+  client.user.setActivity(`Pokemon Hunt`, { url: "https://www.twitch.tv/hypebot"}, { type: "STREAMING"});
 });
 
 client.on('error', error => {
