94a6f461b239d4908df6b3497f57a5032e7dba49
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-09-19 12:11:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ client.on('message', message => {
                           text: result,
                           alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
                         }, img.bitmap.width, img.bitmap.height);
-                        img.print(font2, 0, img.bitmap.height - 200, {
+                        img.print(font2, 0, img.bitmap.height - 100, {
                           text: "WANT THIS BOT IN YOUR SERVER? GO TO https://discord.gg/7pRT8hX WILL GET INVITE LINK.",
                           alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
                         }, img.bitmap.width, img.bitmap.height);
